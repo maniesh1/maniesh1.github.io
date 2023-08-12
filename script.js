62557279476e10fabc5f16e95544009f0553d6c4
Manish Kumar <96045279+maniesh1@users.noreply.github.com>
2023-08-13 02:09:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "Developer", "Mentor","Data Analyst"],
+        strings: ["Student", "Developer", "Mentor","Data Engineer", "AI/ML Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
